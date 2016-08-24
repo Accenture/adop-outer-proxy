98b34ee1386fe9bca1e75d4cb442cf1555530ce2
Anton Kaspiarovich <anton.kaspiarovich@accenture.com>
2016-08-24 09:34:39 +0300

    Added resolver instruction to fix Amazon ELB DNS

--- servers/scripts/run.sh
+++ servers/scripts/run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-grep 'upstream target' /usr/local/nginx/sites-enabled/base.conf || \
-	(echo "upstream target { server $TARGET:80; }" && cat /usr/local/nginx/sites-enabled/base.conf) > base.conf.new
-
-mv base.conf.new /usr/local/nginx/sites-enabled/base.conf
+grep 'TARGET:80' /usr/local/nginx/sites-enabled/base.conf && \
+    sed -i "s/TARGET:80/$TARGET:80/g" /usr/local/nginx/sites-enabled/base.conf
 
 /usr/local/nginx/sbin/nginx -g 'daemon off;'
